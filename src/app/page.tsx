1f1c4c2c7719ad946e1bcc6da616a35bda30a2a7
Gionata Mettifogo <gmettifogo@gmail.com>
2024-04-11 09:52:03 +0200

    Changed version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default async function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;<code className={styles.code}>src/app/page.tsx</code> v1.0.2
+          Get started by editing&nbsp;<code className={styles.code}>src/app/page.tsx</code> v1.0.3
         </p>
         <div>
           <a href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
